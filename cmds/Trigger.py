e4cbd6487e12baaf653360b3d91d814692875831
s102345 <Josephchang102345@gmail.com>
2020-07-29 20:45:31 +0800

    Mei_Poo

--- cmds/Trigger.py
+++ cmds/Trigger.py
@@ -16,6 +16,8 @@ class Trigger(Cog_Extension):
             await msg.channel.send('w三小')
         if msg.content.startswith('嘛') and msg.author != self.bot.user:
             await msg.channel.send('嘛三小')
+        if msg.content=='咩噗' and msg.author != self.bot.user:
+            await msg.channel.send('https://media.discordapp.net/attachments/736482284508676178/738000488359919636/unknown.png')
 
 def setup(bot):
     bot.add_cog(Trigger(bot))
